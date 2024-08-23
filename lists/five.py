00579039e84f82944d1ce8470f65606f69e4e91f
venkatrohith8121 <venkatrohithkandloori@gmail.com>
2024-08-23 12:40:38 +0530

    list

---
+++ lists/five.py
@@ -0,0 +1,19 @@
+enames=[10,20,30,40,10,20,213,331,10]
+print(enames.count(10))
+
+# reaad
+# len()
+# count()
+# index()
+ 
+# update
+# append()
+# insert()
+# extend()
+# copy()
+# reverse()
+# sort()
+
+# delete
+# remove()
+# pop()
---
+++ lists/four.py
@@ -0,0 +1,5 @@
+l1=[]# empty list
+l2=[10,20,30,40,50]
+l3=eval(input("enter the elements:"))
+print(l3)
+print(type(l3))
