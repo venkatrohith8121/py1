53ce936fbcbcaa560fc3e62b412a867aa142df43
venkatrohith8121 <venkatrohithkandloori@gmail.com>
2024-09-12 12:30:04 +0530

    decorator intro

---
+++ deceorator/two.py
@@ -0,0 +1,22 @@
+def login_requried(func):
+    def inner(name,is_login):
+        if is_login==False:
+            print('login is required')
+        else:
+            func(name,is_login)
+    return inner
+
+def home_page(name,login):
+    print('home page')
+def about_page(name,login):
+    print('about page')
+def product_page(name,login):
+    print('product page')
+@login_requried
+def order_page(name,login):
+    print('order page')
+    
+home_page('rahul',False)
+about_page('rahul',False)
+product_page('rahul',False)
+order_page('rahul',False)
